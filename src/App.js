9339f0303ae35d5c5cda210fbba275ff1a0ca6ac
unknown <chesebrojared@gmail.com>
2021-12-29 01:04:55 -0800

    lots of conditional things i do not understand

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import About from './components/About';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
+
+
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
   const [categories] = useState([
     {
       name: 'commercial',
@@ -20,16 +23,21 @@ function App() {
   return (
     <div>
       <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <div>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
-        </div>
+      {!contactSelected ? (
+      <>
+      <Gallery currentCategory={currentCategory}></Gallery>
+      <About></About>
+      </>
+      ) : (
+      <ContactForm></ContactForm>
+  )}
       </main>
     </div>
   );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,31 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
+
 
 function ContactForm() {
+    
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    const [errorMessage, setErrorMessage] = useState('');
     const { name, email, message } = formState;
 
-    function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
-    }
+    const handleChange = (e) => {
+        if (e.target.name === 'email') {
+          const isValid = validateEmail(e.target.value);
+          if (!isValid) {
+            setErrorMessage('Your email is invalid.');
+          } else {
+            setErrorMessage('');
+          }
+        } else {
+          if (!e.target.value.length) {
+            setErrorMessage(`${e.target.name} is required.`);
+          } else {
+            setErrorMessage('');
+          }
+        }
+      };
+    
 
     function handleSubmit(e) {
         e.preventDefault();
@@ -14,6 +33,7 @@ function ContactForm() {
     }
     
     console.log(formState);
+    
     return (
         <section>
             <h1>Contact me</h1>
@@ -30,6 +50,11 @@ function ContactForm() {
                     <label htmlFor='message'>Message:</label>
                     <textarea name='message' defaultValue={message} onChange={handleChange} rows='5' />
                 </div>
+                {errorMessage && (
+                    <div>
+                        <p className='error-text'>{errorMessage}</p>
+                    </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>
